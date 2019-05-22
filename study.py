018acb05e0bae0fac9c6993f2762b9fb908950b8
pl12345678 <50896382+pl12345678@users.noreply.github.com>
2019-05-22 15:03:30 +0800

    Add files via upload

---
+++ study.py
@@ -0,0 +1,34 @@
+
+# def  math_jc(n):
+#     s = 1
+#     i=1
+#     for i in range(1,n+1):
+#         s=s*i
+#     return s
+# print(math_jc(5))
+
+
+# users = {
+#     'A':{
+#     'first':'yu',
+#     'last':'lei',
+#     'location':'hs',
+#     },
+#     'B':{
+#     'first': 'liu',
+#     'last': 'wei',
+#     'location': 'hs',
+#     },
+# }
+# users['A']['age']=23
+# users['B']['age']=35
+# print(users)
+# print(users['A'])
+# print(users['B'])
+# print ('a name is:',users['A']['first']+users['A']['last'],'A are is :',users['A']['location'])
+# print(users['B']['first']+users['B']['last'],users['B']['location'])
+#
+# print('c:\\log\\new.txt')
+
+
+
